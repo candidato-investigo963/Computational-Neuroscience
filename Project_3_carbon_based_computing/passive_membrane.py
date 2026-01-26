391c16f490d1442df4cebb3fdd6cc7b4fcbb469a
candidato-investigo963 <candidato.investigo963@gmail.com>
2026-01-26 03:14:18 +0100

    Update passive_membrane.py

--- Project_3_carbon_based_computing/passive_membrane.py
+++ Project_3_carbon_based_computing/passive_membrane.py
@@ -3,56 +3,67 @@ from __future__ import print_function
 Created on Wed Apr 22 15:53:00 2015
 
 Charging and discharging curves for passive membrane patch
-R Rao 2007
 
-translated to Python by rkp 2015
 """
 
+# Import necessary libraries
 import numpy as np
 import matplotlib.pyplot as plt
 
-# input current
-I = 10 # nA
+# Input current (in nanoamps)
+I = 10 #
 
-# capacitance and leak resistance
+C = 0.1 # Membrane capacitance in nF
+R = 100 # Leak resistance in MOhms
 
-C = 0.1 # nF
-R = 100 # M ohms
-tau = R*C # = 0.1*100 nF-Mohms = 100*100 pF Mohms = 10 ms
+# Theoretical membrane time constant tau = R * C
+tau = R*C
 print('C = %.3f nF' % C)
 print('R = %.3f M ohms' % R)
 print('tau = %.3f ms' % tau)
 print('(Theoretical)')
 
-# membrane potential equation dV/dt = - V/RC + I/C
+# Membrane equation:
+# dV/dt = -V/(R*C) + I/C
 
-tstop = 150 # ms
+# Total simulation time in ms
+tstop = 150 
 
-V_inf = I*R # peak V (in mV)
-tau = 0 # experimental (ms)
+# Theoretical steady-state voltage
+V_inf = I * R  # Peak membrane voltage in mV
 
-h = 0.2 # ms (step size)
+# Variable to store experimentally measured time constant
+tau = 0 # Experimental tau (ms)
 
-V = 0 # mV
-V_trace = [V] # mV
+
+# Time step for numerical integration
+h = 0.2  # ms
+
+V = 0  # Initial membrane voltage in mV
+V_trace = [V]  # Store voltage values over time
+
 
 for t in np.arange(h, tstop, h):
 
-   # Euler method: V(t+h) = V(t) + h*dV/dt
+   # Euler method to update membrane voltage
+   # dV/dt = -V/(R*C) + I/C
    V = V +h*(- (V/(R*C)) + (I/C))
 
-   # Verify membrane time constant
+   # Estimate the experimental time constant
+   # Tau is reached when voltage reaches ~63.2% of V_inf
    if (not tau and (V > 0.6321*V_inf)):
      tau = t
      print('tau = %.3f ms' % tau)
      print('(Experimental)')
 
-   
-   # Stop current injection 
+   # Turn off the input current after 60% of the simulation time
    if t >= 0.6*tstop:
      I = 0
-
+      
+   # Store voltage value
    V_trace += [V]
+   
+   # Plot the voltage trace every 10 ms
    if t % 10 == 0:
        plt.plot(np.arange(0,t+h, h), V_trace, color='r')
        plt.xlim(0, tstop)
